907dd41520022cdb4e0e705446d3fd0d6cf72a8b
Matt <Blade2021@live.com>
2022-05-18 16:53:00 -0400

    Build 3.2.0
    - Update to alpha v12
    - Work on OBS WS

---
+++ src/main/java/rsystems/objects/StreamAdvert.java
@@ -0,0 +1,12 @@
+package rsystems.objects;
+
+public class StreamAdvert {
+
+    private int id;
+    private String sceneName;
+    private String sourceName;
+    private int cost;
+    private int cooldown;
+
+
+}
