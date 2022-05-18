d2478fdbcebcee4a81c852615eced7e203368f76
Matt <Blade2021@live.com>
2022-05-18 16:55:43 -0400

    Build 3.2.0
    - Update to alpha v12
    - Work on OBS WS

--- src/main/java/rsystems/objects/StreamAdvert.java
+++ src/main/java/rsystems/objects/StreamAdvert.java
@@ -8,5 +8,45 @@ public class StreamAdvert {
     private int cost;
     private int cooldown;
 
+    public StreamAdvert(int id, String sceneName, String sourceName) {
+        this.id = id;
+        this.sceneName = sceneName;
+        this.sourceName = sourceName;
+    }
 
+    public StreamAdvert(int id, String sceneName, String sourceName, int cost, int cooldown) {
+        this.id = id;
+        this.sceneName = sceneName;
+        this.sourceName = sourceName;
+        this.cost = cost;
+        this.cooldown = cooldown;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSceneName() {
+        return sceneName;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
 }
